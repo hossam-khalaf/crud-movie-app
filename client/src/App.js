3698eee7cc34cd386264b66533c369dd7593fb0f
hossam-khalaf <hossamhassan303089@gmail.com>
2020-12-30 07:57:45 +0200

    setting up express server

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>CRUD APP</h1>
-			<label htmlFor=''> Movie</label>
-			<input type='text' name='movieName' />
-			<label htmlFor=''> Review</label>
-			<input type='text' name='movieReview' />
+			<form className='form'>
+				<label htmlFor=''> Movie Name:</label>
+				<input type='text' name='movieName' />
+				<label htmlFor=''> Review:</label>
+				<input type='text' name='movieReview' />
+				<button>Submit</button>
+			</form>
 		</div>
 	)
 }
