286ac2dddcf483e0929a12c958efeb6a6468ee74
hossam-khalaf <hossamhassan303089@gmail.com>
2020-12-30 07:31:36 +0200

    basic react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>CRUD APP</h1>
+			<label htmlFor=''> Movie</label>
+			<input type='text' name='movieName' />
+			<label htmlFor=''> Review</label>
+			<input type='text' name='movieReview' />
+		</div>
+	)
+}
+
+export default App
